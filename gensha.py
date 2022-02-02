946066eac001e540f75b882b37312160a863205e
RajSolai <msraj085@gmail.com>
2022-02-02 07:32:46 +0530

    completed script for sha gen

--- gensha.py
+++ gensha.py
@@ -1,2 +1,12 @@
+import subprocess
+
 langs = ['tam','eng','hin','jpn','rus','fra','tha','ara','chi_sim','spa']
 
+for lang in langs :
+    file_url = 'https://github.com/tesseract-ocr/tessdata_best/raw/main/'+lang+'.traineddata'
+    subprocess.run(['wget',file_url,'-P','/tmp'])
+
+for lang in langs :
+    file_path = '/tmp/'+lang+'.traineddata'
+    subprocess.run(['sha256sum',file_path])
+    
